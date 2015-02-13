1a8527ef22652ebe69fb40d86ed6abdc5de70491
Manabu Ori <manabu.ori+github@gmail.com>
2015-02-13 11:54:23 +0900

    refactor command line options.

--- fetch_rh_docs.py
+++ fetch_rh_docs.py
@@ -86,23 +86,25 @@ Detailed options -h or --help'''.format(__file__)
     parser = ArgumentParser(description=desc)
     parser.add_argument('-u', '--username', type=str, dest='username', help='RHN username')
     parser.add_argument('-P', '--password', type=str, dest='password', help='RHN password')
-    parser.add_argument('-s', '--show', action='store_true', dest='show_flag', help='only print urls.')
-    parser.add_argument('-e', '--epub', action='store_true', dest='epub', help='download epub files')
+    parser.add_argument('-l', '--list', action='store_true', dest='list_only', help='only print list of urls.')
     parser.add_argument('-p', '--pdf', action='store_true', dest='pdf', help='download pdf files')
-    parser.add_argument('-S', '--single-html', action='store_true', dest='single_html', help='download single html files')
-    parser.add_argument('-l', '--external-link', action='store_true', dest='external_link', help='download exteral links')
+    #parser.add_argument('-e', '--epub', action='store_true', dest='epub', help='download epub files')
+    #parser.add_argument('-S', '--single-html', action='store_true', dest='single_html', help='download single html files')
+    parser.add_argument('-k', '--kb', action='store_true', dest='kb', help='download kb')
+    parser.add_argument('-c', '--convert-to-pdf', action='store_true', dest='convert_to_pdf', help='convert html to pdf')
     parser.add_argument('url', nargs='?')
     args = parser.parse_args()
-    print "(debug) %s: %s" % ('username', args.username)
-    print "(debug) %s: %s" % ('password', args.password)
-    print "(debug) %s: %s" % ('show', args.show_flag)
-    print "(debug) %s: %s" % ('epub', args.epub)
-    print "(debug) %s: %s" % ('pdf', args.pdf)
-    print "(debug) %s: %s" % ('single_html', args.single_html)
-    print "(debug) %s: %s" % ('external_link', args.external_link)
-    print "(debug) %s: %s" % ('url', args.url)
     if args.url is None:
         args.url = top_url
+    print "(debug) %s: %s" % ('username', args.username)
+    print "(debug) %s: %s" % ('password', args.password)
+    print "(debug) %s: %s" % ('list', args.list_only)
+    print "(debug) %s: %s" % ('pdf', args.pdf)
+    #print "(debug) %s: %s" % ('epub', args.epub)
+    #print "(debug) %s: %s" % ('single_html', args.single_html)
+    print "(debug) %s: %s" % ('kb', args.kb)
+    print "(debug) %s: %s" % ('convert_to_pdf', args.convert_to_pdf)
+    print "(debug) %s: %s" % ('url', args.url)
     return args
 
 def main():
@@ -115,18 +117,18 @@ def main():
         print "# pdf"
         for url in parse_pdf_urls(content):
             print url
-            if args.show_flag:
+            if args.list_only:
                 continue
             print "  * Downloading..."
             cmd = "/usr/bin/curl -O '%s' > /dev/null 2>&1" % url
             #print "      ", cmd
             subprocess.call(cmd, shell=True)
 
-    if args.external_link:
+    if args.kb:
         print "# kb"
         for url in parse_kb_urls(content):
             print url
-            if args.show_flag:
+            if args.list_only:
                 continue
             title, content = fetch_kb_content(session, url, args.username, args.password)
             print "  title:", title
@@ -134,11 +136,12 @@ def main():
             f = open(title + '.html', 'w')
             f.write(content)
             f.close()
-            print "  * Converting to pdf..."
-            cmd = "/usr/local/bin/wkhtmltopdf --load-error-handling ignore --load-media-error-handling ignore --disable-external-links '%s' '%s' > /dev/null 2>&1" % (title + '.html', title + '.pdf')
-            #subprocess.call(cmd.strip().split(" "))
-            #print "      ", cmd
-            subprocess.call(cmd, shell=True)
+            if args.convert_to_pdf:
+                print "  * Converting to pdf..."
+                cmd = "/usr/local/bin/wkhtmltopdf --load-error-handling ignore --load-media-error-handling ignore --disable-external-links '%s' '%s' > /dev/null 2>&1" % (title + '.html', title + '.pdf')
+                #subprocess.call(cmd.strip().split(" "))
+                #print "      ", cmd
+                subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
     main()
